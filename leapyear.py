e4c2be6a91425bb82a7f06d862783f421e7d90b0
katie <katieholmes241@gmail.com>
2021-05-29 15:46:32 -0700

    leapyear initial code written, ran and failed first test

---
+++ leapyear.py
@@ -0,0 +1,2 @@
+def isleapyear(year):
+    pass
--- leapyear_test.py
+++ leapyear_test.py
@@ -3,7 +3,7 @@ import leapyear
 
 class TestCase (unittest.TestCase):
     def test_1(self):
-        self.assertEqual(leapyear.isleapyear(a),0)
+        self.assertEqual(leapyear.isleapyear("a"),0)
 
 if __name__ == '__main__': 
     unittest.main(verbosity=2) 
