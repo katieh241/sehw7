b45dc88a678d2836abd9a0ca673dedc3fc7010d0
katie <katieholmes241@gmail.com>
2021-05-29 15:57:51 -0700

    created second test, failed

--- leapyear_test.py
+++ leapyear_test.py
@@ -5,5 +5,8 @@ class TestCase (unittest.TestCase):
     def test_1(self):
         self.assertEqual(leapyear.isleapyear("a"),0)
 
+    def test_1(self):
+        self.assertEqual(leapyear.isleapyear(2004),"true")
+
 if __name__ == '__main__': 
     unittest.main(verbosity=2) 
