a47dbf56d0b1c49a35d4e0a1bbfb1caf2502583c
katie <katieholmes241@gmail.com>
2021-05-29 21:21:32 -0700

    created fourth test for fizzbuzz, fail

--- fizzbuzz_test.py
+++ fizzbuzz_test.py
@@ -11,5 +11,8 @@ class TestCase (unittest.TestCase):
     def test_3(self):
         self.assertEqual(fizzbuzz.fizz_buzz(25),"buzz") 
 
+    def test_4(self):
+        self.assertEqual(fizzbuzz.fizz_buzz(15),"fizzbuzz")
+
 if __name__ == '__main__': 
     unittest.main(verbosity=2) 
