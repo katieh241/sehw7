22f4dbe785bdc6d766328df190da0e184e31f786
katie <katieholmes241@gmail.com>
2021-05-29 21:26:43 -0700

    comment on fizzbuzz.py regarding function

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -10,3 +10,8 @@ def fizz_buzz(i):
         return str
     else:
         return i
+    
+    #now all numbers are converted to fizz, buzz, fizzbuzz or their original 
+    #state based on whether or not they are divisible by 3, 5, both or neither. 
+    #In order to fully implement this function, just create a for loop to print 
+    #numbers 1-100, this testing just confirms that each number is converted correctly
