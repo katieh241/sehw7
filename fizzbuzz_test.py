dc5314771df87acaf7849c18cb83d4ba69ee8ec4
katie <katieholmes241@gmail.com>
2021-05-29 21:14:33 -0700

    created second test for fizzbuzz, fail

--- fizzbuzz_test.py
+++ fizzbuzz_test.py
@@ -5,5 +5,8 @@ class TestCase (unittest.TestCase):
     def test_1(self):
         self.assertEqual(fizzbuzz.fizz_buzz(100),100)
 
+    def test_2(self):
+        self.assertEqual(fizzbuzz.fizz_buzz(9),"fizz")
+
 if __name__ == '__main__': 
     unittest.main(verbosity=2) 
